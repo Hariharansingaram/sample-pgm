4ca5db8ecee55c4de82778f65e0c8382e01035cf
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-01-31 14:15:51 +0530

    Create 2.py
    change the A to 1
    b to 0

---
+++ 2.py
@@ -0,0 +1,13 @@
+a=int(input())
+l=[]
+for i in range(0,a):
+    c=input()
+    if c =="A":
+        l.append(1)
+    elif c =="B":
+        l.append(0) 
+    else:
+        l.append(c)
+print(l) 
+     
+        
