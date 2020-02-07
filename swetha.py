c838badd6035e7acd406cbfb2fed4671bfda08f9
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-07 16:00:32 +0530

    Create swetha.py

---
+++ swetha.py
@@ -0,0 +1,13 @@
+n="Sweth123@007a@"
+l1=[]
+l2=[]
+l3=[]
+for i in n:
+    if i.isalpha():
+        l1.append(i)
+    elif i.isdigit():
+        l2.append(i)
+    else:
+        l3.append(i)
+for j in 
+    
