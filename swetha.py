eb11a486196f10386178261b02fd375ba62a0e58
Hariharansingaram <52225820+Hariharansingaram@users.noreply.github.com>
2020-02-08 15:13:24 +0530

    Update swetha.py

--- swetha.py
+++ swetha.py
@@ -1,4 +1,4 @@
-n="Sweth123@007a@"
+n="Sweth129@001a@@"
 l1=[]
 l2=[]
 l3=[]
@@ -9,5 +9,23 @@ for i in n:
         l2.append(i)
     else:
         l3.append(i)
-for j in 
-    
+c=len(l3)
+l4=[]
+e=[]
+o=[]
+for i in range(len(l2)):
+    l4.append(int(l2[i]))
+d=len(l4)
+for i in range(d):
+    if l4[i]%2==0:
+        e.append(l4[i])
+    else:
+        o.append(l4[i])
+s=""
+if c%2==0:
+    for i in range(0,len(e)):
+        s=s+str(e[i])+str(o[i])
+else:
+    for i in range(0,len(o)):
+        s=s+str(o[i])+str(e[i])
+        
